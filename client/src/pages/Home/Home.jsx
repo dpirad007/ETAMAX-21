79ffc5123ca95c4db922a2e9ed8753ebe226f854
Dion Pinto <dionpinto000@gmail.com>
2021-01-27 16:46:06 +0530

    Dummy home added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+
+import "./Home.css";
+
+const { Header, Sider, Content } = Layout;
 
 const Home = () => {
-  return <div>yo</div>;
+  const [collapse, setCollapse] = useState(false);
+
+  const toggle = () => {
+    setCollapse(!collapse);
+  };
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider trigger={null} collapsible collapsed={collapse}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            Events
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            Profile
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(
+            collapse ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => toggle(),
+            }
+          )}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          Events will be displayed here
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default Home;
