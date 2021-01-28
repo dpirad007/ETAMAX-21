2389072b6570f66a54bb923e2d2e334d392d7a25
Dion Pinto <dionpinto000@gmail.com>
2021-01-28 21:58:41 +0530

    profile added and some warnings fixed

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,9 +1,41 @@
 import React, { Fragment } from "react";
+import { Progress, Steps, Switch } from "antd";
 
 import "./Profile.css";
 
+const { Step } = Steps;
+
 const Profile = () => {
-  return <Fragment>Profile</Fragment>;
+  return (
+    <Fragment>
+      <div className="p-main">
+        <div className="p-circBar">
+          <Progress type="circle" percent={80} />
+        </div>
+        <div className="p-steps">
+          <Steps current={2}>
+            <Step title="Select Events" description="oowaeii" />
+            <Step title="Meet Criterion" description="oowaeiaa" />
+            <Step title="Paid" description="oowaeioo" />
+          </Steps>
+        </div>
+      </div>
+      <div className="p-table">
+        <div className="p-cri1">
+          <Switch defaultChecked />
+        </div>
+        <div className="p-cri2">
+          <Switch defaultChecked />
+        </div>
+        <div className="p-cri3">
+          <Switch defaultChecked />
+        </div>
+        <div className="p-cri1-txt">Cultural</div>
+        <div className="p-cri2-txt">Technical</div>
+        <div className="p-cri3-txt">Donno</div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Profile;
