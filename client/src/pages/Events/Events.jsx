f16c42d72fe42b2ac3d8a6feeafaad4b7e8b37f5
Dion Pinto <dionpinto000@gmail.com>
2021-01-31 18:57:17 +0530

    dropdown added on events

--- client/src/pages/Events/Events.jsx
+++ client/src/pages/Events/Events.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Menu, Dropdown, Button, Space } from "antd";
 
 //components
 import HomeCard from "../../components/Home/HomeCard/HomeCard";
 
 import "./Events.css";
 
+const menu = (
+  <Menu>
+    <Menu.Item>Day1</Menu.Item>
+    <Menu.Item>Day2</Menu.Item>
+    <Menu.Item>Day3</Menu.Item>
+  </Menu>
+);
+
 const Events = () => {
   return (
-    <div className="e-main">
-      <div className="e-main-item">
-        <HomeCard />
+    <Fragment>
+      <div className="e-dropdown">
+        <Dropdown overlay={menu} placement="bottomLeft">
+          <Button>Categories</Button>
+        </Dropdown>
       </div>
-      <div className="e-main-item">
-        <HomeCard />
+      <div className="e-main">
+        <div className="e-main-item">
+          <HomeCard />
+        </div>
+        <div className="e-main-item">
+          <HomeCard />
+        </div>
+        <div className="e-main-item">
+          <HomeCard />
+        </div>
+        <div className="e-main-item">
+          <HomeCard />
+        </div>
       </div>
-      <div className="e-main-item">
-        <HomeCard />
-      </div>
-      <div className="e-main-item">
-        <HomeCard />
-      </div>
-    </div>
+    </Fragment>
   );
 };
 
