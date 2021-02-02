fca0bc44a284c4febadd834868cfa00fa76a569d
Dion Pinto <dionpinto000@gmail.com>
2021-02-02 16:47:15 +0530

    home timeline functionality added

--- client/src/components/Home/HomeTimeLine/HomeTimeLine.jsx
+++ client/src/components/Home/HomeTimeLine/HomeTimeLine.jsx
@@ -36,110 +36,339 @@ const importantIconStyles = {
   background: "#7cffb3",
 };
 
-const HomeTimeLine = () => {
-  return (
-    <Fragment>
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={contentStyles}
-          contentArrowStyle={rightArrow}
-          date="2011 - present"
-          iconStyle={normalIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentArrowStyle={leftArrow}
-          date="2010 - 2011"
-          iconStyle={normalIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={normalIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2006 - 2008"
-          iconStyle={importantIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="April 2013"
-          iconStyle={importantIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={importantIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Agile Development Scrum Master
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={normalIconStyles}
-          icon={<ClockCircleOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          iconStyle={normalIconStyles}
-          icon={<ClockCircleOutlined />}
-        />
-      </VerticalTimeline>
-    </Fragment>
-  );
+const HomeTimeLine = ({ selectedDay }) => {
+  let finalTimeline = null;
+
+  switch (selectedDay) {
+    case 1:
+      finalTimeline = (
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={contentStyles}
+            contentArrowStyle={rightArrow}
+            date="2011 - present"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Day1</h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, Project
+              Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentArrowStyle={leftArrow}
+            date="2010 - 2011"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Art Director</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, SEO, Online
+              Marketing
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2008 - 2010"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Los Angeles, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2006 - 2008"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="April 2013"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Content Marketing for Web, Mobile and Social Media
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Online Course
+            </h4>
+            <p>Strategy, Social Media</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="November 2012"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Agile Development Scrum Master
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Certification
+            </h4>
+            <p>Creative Direction, User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2002 - 2006"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Bachelor of Science in Interactive Digital Media Visual Imaging
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Bachelor Degree
+            </h4>
+            <p>Creative Direction, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          />
+        </VerticalTimeline>
+      );
+      break;
+
+    case 2:
+      finalTimeline = (
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={contentStyles}
+            contentArrowStyle={rightArrow}
+            date="2011 - present"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Day2</h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, Project
+              Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentArrowStyle={leftArrow}
+            date="2010 - 2011"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Art Director</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, SEO, Online
+              Marketing
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2008 - 2010"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Los Angeles, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2006 - 2008"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="April 2013"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Content Marketing for Web, Mobile and Social Media
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Online Course
+            </h4>
+            <p>Strategy, Social Media</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="November 2012"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Agile Development Scrum Master
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Certification
+            </h4>
+            <p>Creative Direction, User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2002 - 2006"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Bachelor of Science in Interactive Digital Media Visual Imaging
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Bachelor Degree
+            </h4>
+            <p>Creative Direction, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          />
+        </VerticalTimeline>
+      );
+      break;
+
+    case 3:
+      finalTimeline = (
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={contentStyles}
+            contentArrowStyle={rightArrow}
+            date="2011 - present"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Day3</h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, Project
+              Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentArrowStyle={leftArrow}
+            date="2010 - 2011"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Art Director</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, SEO, Online
+              Marketing
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2008 - 2010"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Los Angeles, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2006 - 2008"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="April 2013"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Content Marketing for Web, Mobile and Social Media
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Online Course
+            </h4>
+            <p>Strategy, Social Media</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="November 2012"
+            iconStyle={importantIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Agile Development Scrum Master
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Certification
+            </h4>
+            <p>Creative Direction, User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2002 - 2006"
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Bachelor of Science in Interactive Digital Media Visual Imaging
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Bachelor Degree
+            </h4>
+            <p>Creative Direction, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            iconStyle={normalIconStyles}
+            icon={<ClockCircleOutlined />}
+          />
+        </VerticalTimeline>
+      );
+      break;
+
+    default:
+      finalTimeline = null;
+  }
+
+  return <Fragment>{finalTimeline}</Fragment>;
 };
 
 export default HomeTimeLine;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,31 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
-import { Menu, Dropdown, Button } from "antd";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 import HomeCarousel from "../../components/Home/HomeCarousel/HomeCarousel";
 import HomeTimeLine from "../../components/Home/HomeTimeLine/HomeTimeLine";
 import "./Home.css";
 
-const menu = (
-  <Menu>
-    <Menu.Item>Day 1</Menu.Item>
-    <Menu.Item>Day 1</Menu.Item>
-    <Menu.Item>Day 1</Menu.Item>
-    <Menu.Item danger>a danger item</Menu.Item>
-  </Menu>
-);
+const options = [
+  { value: 1, label: "First Day" },
+  { value: 2, label: "Second Day" },
+  { value: 3, label: "Third Day" },
+];
 
 const Home = () => {
+  const [selectedDay, setSelectedDay] = useState(1);
+
+  useEffect(() => {
+    console.log(selectedDay);
+  }, [selectedDay]);
+
   return (
     <Fragment>
       <div className="ho-dropdown">
-        <Dropdown overlay={menu}>
-          <Button>Day</Button>
-        </Dropdown>
+        <Dropdown
+          className="ho-dropdown-item"
+          options={options}
+          onChange={(_onSelect) => setSelectedDay(_onSelect.value)}
+          placeholder="Select Day"
+        />
       </div>
       <div className="ho-main">
         <HomeCarousel />
-        <HomeTimeLine />
+        <HomeTimeLine selectedDay={selectedDay} />
       </div>
     </Fragment>
   );
