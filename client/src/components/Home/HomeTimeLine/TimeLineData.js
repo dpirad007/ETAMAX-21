fb7eabb8d1ba1e223302ccb8818a4de8d9cd6491
rohangawhade <gawhaderohan@gmail.com>
2021-02-03 21:16:30 +0530

    Added Events in Day 1 Timeline

--- client/src/components/Home/HomeTimeLine/TimeLineData.js
+++ client/src/components/Home/HomeTimeLine/TimeLineData.js
@@ -1,15 +1,129 @@
 export const timeLineDataDay1 = [
   {
+    date: "8:00 AM",
+    h3: "Solo Dance",
+    h4: "Solo",
+    p: "Cultural Event"
+  },
+  {
     date: "9:00 AM",
-    h3: "Day 1 event",
-    h4: "Team event",
-    p: "description of event",
+    h3: "Duet Dance",
+    h4: "Team of 2",
+    p: "Cultural Event",
+  },
+  {
+    date: "9:00 AM",
+    h3: "Bug Hunter",
+    h4: "Solo",
+    p: "Technical Event",
   },
   {
     date: "10:00 AM",
-    h3: "Day 1 event 2",
-    h4: "Team event",
-    p: "description of event",
+    h3: "Mehendi",
+    h4: "Solo",
+    p: "Cultural Event",
+  },
+  {
+    date: "10:00 AM",
+    h3: "Tech Debate",
+    h4: "Team of 2",
+    p: "Technical Event",
+  },
+  {
+    date: "11:00 AM",
+    h3: "Solo Singing",
+    h4: "Solo",
+    p: "Cultural Event",
+  },
+  {
+    date: "11:00 AM",
+    h3: "QuizX",
+    h4: "Team of 2",
+    p: "Technical Event",
+  },
+  {
+    date: "12:00 PM",
+    h3: "Duet Singing",
+    h4: "Team of 2",
+    p: "Cultural Event",
+  },
+  {
+    date: "12:00 PM",
+    h3: "Hackathon",
+    h4: "Team of 5",
+    p: "Technical Event",
+  },
+  {
+    date: "01:00 PM",
+    h3: "Words Worth",
+    h4: "Team of 2",
+    p: "Cultural Event",
+  },
+  {
+    date: "01:00 PM",
+    h3: "Ideathon",
+    h4: "Team of 3",
+    p: "Technical Event",
+  },
+  {
+    date: "02:00 PM",
+    h3: "Mono Acting",
+    h4: "Solo",
+    p: "Cultural Event",
+  },
+  {
+    date: "03:00 PM",
+    h3: "Short Film",
+    h4: "Team event(max 20)",
+    p: "Cultural Event",
+  },
+  {
+    date: "04:00 PM",
+    h3: "Treasure Hunt",
+    h4: "Team of 4",
+    p: "Cultural Event",
+  },
+  {
+    date: "05:00 PM",
+    h3: "Fashion Event",
+    h4: "Solo",
+    p: "Cultural Event",
+  },
+  {
+    date: "06:00 PM",
+    h3: "Yoga",
+    h4: "Solo",
+    p: "Cultural Seminar",
+  },
+  {
+    date: "07:00 PM",
+    h3: "Zumba",
+    h4: "Solo",
+    p: "Cultural Seminar",
+  },
+  {
+    date: "07:00 PM",
+    h3: "Web Technologies",
+    h4: "Solo",
+    p: "Technical Seminar",
+  },
+  {
+    date: "07:00 PM",
+    h3: "CLOUD COMPUTING MEETS BIG DATA",
+    h4: "Solo",
+    p: "Technical Seminar",
+  },
+  {
+    date: "07:00 PM",
+    h3: "ELECTRIC VEHICLES",
+    h4: "Solo",
+    p: "Technical Seminar",
+  },
+  {
+    date: "07:00 PM",
+    h3: "SECRETS OF BRANDING",
+    h4: "Solo",
+    p: "Technical Seminar",
   },
 ];
 
@@ -18,7 +132,7 @@ export const timeLineDataDay2 = [
     date: "9:00 AM",
     h3: "Day 2 event",
     h4: "Team event",
-    p: "description of event",
+    p: "Cultural Event",
   },
 ];
 
@@ -27,6 +141,6 @@ export const timeLineDataDay3 = [
     date: "9:00 AM",
     h3: "Day 3 event",
     h4: "Team event",
-    p: "description of event",
+    p: "Cultural Event",
   },
 ];
