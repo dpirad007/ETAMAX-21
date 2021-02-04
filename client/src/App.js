d8ddb93f71617315fdd21bace616600d41b4a0c5
Dion Pinto <dionpinto000@gmail.com>
2021-02-04 15:06:50 +0530

    Revert to history.push working state

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 import Login from "./pages/Login/Login";
-// import Register from "./pages/Register/Register";
+import Register from "./pages/Register/Register";
 
 import Events from "./pages/Events/Events";
 
