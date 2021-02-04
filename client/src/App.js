9ef85389fc3c6c720614aebd172a3f519d7f85c2
Dion Pinto <dionpinto000@gmail.com>
2021-02-04 14:31:20 +0530

    register route removed

--- client/src/App.js
+++ client/src/App.js
@@ -54,7 +54,7 @@ function App() {
                 <PrivateRoute exact path="/" component={Home} />
                 <PrivateRoute exact path="/profile" component={Profile} />
                 <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
+                {/*<Route exact path="/register" component={Register} />*/}
                 <PrivateRoute exact path="/events" component={Events} />
               </Switch>
             </Content>
