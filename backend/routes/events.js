038d4edd02bad5df227cc6c769316b6709217f91
srividya-p <srividya.ssa@gmail.com>
2021-02-07 18:43:56 +0530

    res.send([])

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -79,12 +79,12 @@ router.post('/register-event', authenticate.verifyUser, async (req, res) => {
         let event = await Event.findOne({ eventCode: req.body.eventCode })
         //Does the event have seats left?
         if (event.maxSeats - event.seats === 0) {
-            return res.status(400).send({ 'message': 'This event has no seats left!' })
+            return res.status(400).send([{ 'message': 'This event has no seats left!' }])
         }
         if (event.teamSize === 1) {
             //Has the user already registered for this event?
             if (req.user.events.includes(event._id)) {
-                return res.status(400).send({ 'message': 'You have already registered for event!' })
+                return res.status(400).send([{ 'message': 'You have already registered for event!' }])
             }
 
             //Update User: Update criteria, increase moneyOwed and push event id into events array
@@ -104,12 +104,12 @@ router.post('/register-event', authenticate.verifyUser, async (req, res) => {
             }
             await Event.findOneAndUpdate({ eventCode: req.body.eventCode }, event_update)
 
-            res.status(201).send({ 'message': 'Criteria and Seats updated!' })
+            res.status(201).send([{ 'message': 'Criteria and Seats updated!' }])
 
         } else {
             //Has the current user (leader) already registered for this event?
             if (req.user.events.includes(event._id)) {
-                return res.status(400).send({ 'message': 'You have already registered for event!' })
+                return res.status(400).send([{ 'message': 'You have already registered for event!' }])
             }
 
             //Make an array of all team member Roll Numbers
@@ -121,12 +121,12 @@ router.post('/register-event', authenticate.verifyUser, async (req, res) => {
 
             //Is the team size correct?
             if (event.isTeamSizeStrict && team.length !== event.teamSize) {
-                return res.status(400).send({ 'message': 'This event has a strict team size! Please add more members!' })
+                return res.status(400).send([{ 'message': 'This event has a strict team size! Please add more members!' }])
             }
 
             //Are any of the input roll numbers duplicates?
             if (hasDuplicates(team)) {
-                return res.status(400).send({ 'message': 'Team members have been repeated! Please ensure they are unique!' })
+                return res.status(400).send([{ 'message': 'Team members have been repeated! Please ensure they are unique!' }])
             }
 
             //Have any of the members in the input already registered for this event?
@@ -181,10 +181,10 @@ router.post('/register-event', authenticate.verifyUser, async (req, res) => {
                 await User.findOneAndUpdate({ rollNo: team[i] }, user_update)
             }
 
-            res.status(200).send({ 'message': 'Criteria and Seats updated!' })
+            res.status(200).send([{ 'message': 'Criteria and Seats updated!' }])
         }
     } catch (e) {
-        res.status(400).send({error:e.message})
+        res.status(400).send([{error:e.message}])
     }
 })
 
