7ffa898ba564d130aa7f2889040f79d68b62f58c
Dion Pinto <dionpinto000@gmail.com>
2021-02-08 20:27:09 +0530

    start time added to event card

--- client/src/components/Misc/DetailsModal/DetailsModal.jsx
+++ client/src/components/Misc/DetailsModal/DetailsModal.jsx
@@ -3,6 +3,21 @@ import { Modal, Button } from "antd";
 
 import "./DetailsModal.css";
 
+function tConvert(time) {
+  // Check correct time format and split into components
+  time = time.toString().match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) || [
+    time,
+  ];
+
+  if (time.length > 1) {
+    // If time format correct
+    time = time.slice(1); // Remove full string match value
+    time[5] = +time[0] < 12 ? "AM" : "PM"; // Set AM/PM
+    time[0] = +time[0] % 12 || 12; // Adjust hours
+  }
+  return time.join(""); // return adjusted time or original string
+}
+
 const catTocat = {
   T: "Techincal",
   C: "Cultural",
@@ -12,7 +27,16 @@ const catTocat = {
 const DetailsModal = ({
   changeAddModal,
   addModalVisible,
-  data: { title, description, category, prizeMoney, maxSeats, entryFee, seats },
+  data: {
+    title,
+    description,
+    category,
+    prizeMoney,
+    maxSeats,
+    entryFee,
+    seats,
+    start,
+  },
 }) => {
   return (
     <div>
@@ -34,6 +58,7 @@ const DetailsModal = ({
         <div className="dm-main">
           <div className="dm-title">{title}</div>
           <div className="dm-cat">{catTocat[category]}</div>
+          <div className="dm-cat">Start Time: {tConvert(start.slice(2))}</div>
           <div className="dm-desc">{description}</div>
           <div className="dm-prize">Prize: {prizeMoney[0]}</div>
           <div className="dm-seats">Seats: {seats + "/" + maxSeats}</div>
