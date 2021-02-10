816870acfa0e097fe478cf7c2504ef60a595dda5
srividya-p <srividya.ssa@gmail.com>
2021-02-10 11:46:35 +0530

    removed http-errors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 var path = require('path');
 
-var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -29,22 +28,6 @@ app.use(cors()) // Use this after the variable declaration
 
 app.use('/api/users', usersRouter);
 app.use('/api/events', eventsRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is up and running on port " + process.env.PORT)
