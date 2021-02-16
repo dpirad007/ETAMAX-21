54050f92ce757fdef4f7a9068f2b1f905ac12bcd
Prithviraj2511 <prithvirajpatil2511@gmail.com>
2021-02-16 13:50:56 +0530

    Background added in login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import React, { Fragment } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import "./Login.css";
+import logo from "../../assets/loginbackground.jpeg";
 
 const Login = ({ loginupdater, setProfileCheck, profileCheck }) => {
   let history = useHistory();
@@ -51,65 +52,70 @@ const Login = ({ loginupdater, setProfileCheck, profileCheck }) => {
 
   return (
     <Fragment>
-      <div className="login-form">
-        <Form
-          name="normal_login"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          style={{ padding: "1rem" }}
-        >
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Rollnumber!",
-              },
-            ]}
-          >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="FCRIT Rollnumber"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Password!",
-              },
-            ]}
-          >
-            <Input.Password
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
+      <div className="login-container" style={{ backgroundImage: `url(${logo})` }}>
+        <div className="login-card">
+          <div className="login-form">
+            <Form
+              name="normal_login"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              style={{ padding: "1rem" }}
             >
-              Log in
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Rollnumber!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="FCRIT Rollnumber"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password!",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Log in
             </Button>
-          </Form.Item>
-        </Form>
-      </div>
-      <div className="p-condet-main">
-        <div className="p-condet">For non FCRIT Students please contact:</div>
-        <div className="p-condet">
-          Dylan Pinto: <a href="tel:+917738087256">+91 7738087256</a>
+              </Form.Item>
+            </Form>
+          </div>
+          <div className="p-condet-main">
+            <div className="p-condet">For non FCRIT Students please contact:</div>
+            <div className="p-condet">
+              Dylan Pinto: <a href="tel:+917738087256">+91 7738087256</a>
+            </div>
+            <div className="p-condet">
+              Rohan M: <a href="tel:+919867608101">+91 9867608101</a>
+            </div>
+          </div>
         </div>
-        <div className="p-condet">
-          Rohan M: <a href="tel:+919867608101">+91 9867608101</a>
-        </div>
       </div>
+
     </Fragment>
   );
 };
