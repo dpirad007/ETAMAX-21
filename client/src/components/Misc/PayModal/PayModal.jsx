2d2c400a0f66523e0d98cc0b66759095385d542f
Dion Pinto <dionpinto000@gmail.com>
2021-02-19 12:19:59 +0530

    profile pay details added

---
+++ client/src/components/Misc/PayModal/PayModal.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Modal, Button } from "antd";
+
+import "./PayModal.css";
+
+const PayModal = ({ addModalVisible, changeAddModal }) => {
+  return (
+    <div>
+      <Modal
+        title="Payment Details"
+        centered
+        visible={addModalVisible}
+        onCancel={() => changeAddModal(false)}
+        footer={[
+          <Button
+            key="Close"
+            type="primary"
+            onClick={() => changeAddModal(false)}
+          >
+            Close
+          </Button>,
+        ]}
+      >
+        <h4>IFSC: IOBA 0000596</h4>
+        <h4>Account No: 059601000007942</h4>
+      </Modal>
+    </div>
+  );
+};
+
+export default PayModal;
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import MyEvents from "../../components/Profile/MyEvents";
 import "./Profile.css";
 
+import PayModal from "../../components/Misc/PayModal/PayModal";
+
 const { Step } = Steps;
 
 const Profile = () => {
@@ -15,6 +17,17 @@ const Profile = () => {
   const [totalAmt, settotalAmt] = useState(0);
   const [userName, setUserName] = useState();
   const [criteriaDescription, setDescription] = useState();
+  const [critTrue, setCritTrue] = useState(false);
+
+  const [addModalVisible, setAddModalVisible] = useState(false);
+
+  const changeAddModal = (bool) => {
+    setAddModalVisible(bool);
+  };
+
+  useEffect(() => {
+    console.log(currentCompletion);
+  }, [currentCompletion]);
 
   useEffect(() => {
     axios
@@ -53,6 +66,7 @@ const Profile = () => {
         if (response.data.hasFilledProfile) {
           setCurrent({ per: 33, cur: 1 });
           if (isCriteria) {
+            setCritTrue(true);
             setCurrent({ per: 66, cur: 2 });
             if (response.data.moneyOwed === 0) {
               setCurrent({ per: 100, cur: 3 });
@@ -63,6 +77,7 @@ const Profile = () => {
       .catch((e) => setErr(1));
   }, []);
   const paidDescription = `Payment for events!`;
+
   return (
     <Fragment>
       {err ? (
@@ -99,6 +114,24 @@ const Profile = () => {
         </div>
       )}
 
+      {critTrue ? (
+        <div className="pr-pay-main">
+          <div className="pr-pay-tit">Payment Details</div>
+          <div
+            className="pr-pay-btn"
+            onClick={() => {
+              changeAddModal(true);
+            }}
+          >
+            Open
+          </div>
+        </div>
+      ) : null}
+
+      <PayModal
+        addModalVisible={addModalVisible}
+        changeAddModal={changeAddModal}
+      />
       <MyEvents />
     </Fragment>
   );
